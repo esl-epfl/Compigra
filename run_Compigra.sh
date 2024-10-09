8ba8bb79a6ee4ad7803a32f6462f92a596390c3f
yuxuan wang <yuxwang.1999@hotmail.com>
2024-10-09 17:23:40 +0200

    remove polygeist frontend

--- run_Compigra.sh
+++ run_Compigra.sh
@@ -9,51 +9,6 @@ BENCH_BASE="$POLYGEIST_PATH/../benchmarks"
 # Plugin for modulo scheduling
 MS_PLUGIN=`OPT_PLUGIN_PATH` # optional: set the path to the SAT-MapIt plugin
 
-compile() {
-    local bench_name="$1"
-    local bench_path="$BENCH_BASE/$bench_name"
-    local bench_c="$bench_path/$bench_name.c"
-
-    # Check if the benchmark file exists
-    if [ ! -f "$bench_c" ]; then
-        echo "Error:$bench_c not found."
-        exit 1
-    fi
-
-    # remove the old IR files
-    rm -f "$bench_path/IR/"*
-
-    local f_scf="$bench_path/IR/scf.mlir"
-    local f_cf="$bench_path/IR/cf.mlir"
-    local f_cf_opt="$bench_path/IR/cf_opt.mlir"
-    local f_std="$bench_path/IR/std.mlir"
-    local f_llvm="$bench_path/IR/llvm.mlir"
-
-    # Compile the benchmark using cgeist
-    local include="$POLYGEIST_PATH/llvm-project/clang/lib/Headers/"
-    $POLYGEIST_PATH/build/bin/cgeist "$bench_c" -I "$include" \
-         -function="$bench_name" -S --O3 --memref-fullrank  --canonicalizeiters=10 > "$f_scf"
-    
-    # lower down to cf 
-    $MLIR_OPT --scf-for-loop-canonicalization --canonicalize --convert-scf-to-cf   "$f_scf" > "$f_cf"
-
-    # # rewrite the cf
-    $COMPIGRA_OPT --allow-unregistered-dialect --fix-index-width \
-        --reduce-branches "$f_cf" > "$f_cf_opt"
-
-    # # lower down to standard dialect
-    # # $MLIR_OPT --convert-complex-to-standard "$f_cf_opt" > "$f_std"
-    # $MLIR_OPT --test-lower-to-llvm "$f_cf" > "$f_llvm"
-
-    # Check if the compilation was successful
-    if [ $? -eq 0 ]; then
-        echo "Compilation successful."
-    else
-        echo "Compilation failed."
-    fi
-    return 0
-}
-
 # compile use C->LLVM->MLIR(LLVM)->MLIR(CGRA)
 compile_sat() {
     local bench_name="$1"
